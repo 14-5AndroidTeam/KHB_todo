347b22f9d3878779a083a27d21b0cbc28da959ca
YangNu <bin450579@gmail.com>
2023-01-10 01:49:54 +0900

    Feat. 추가. ListFragment 구현

--- app/src/main/java/com/khb/todo/ListFragment.kt
+++ app/src/main/java/com/khb/todo/ListFragment.kt
@@ -1,59 +1,63 @@
 package com.khb.todo
 
+import android.content.Context
+import android.database.DataSetObserver
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.khb.todo.Data.DTO.User
+import com.khb.todo.ViewModel.ListModel
+import com.khb.todo.databinding.FragmentListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentListBinding
+    private lateinit var adapter: ListviewAdapter
+    private lateinit var user: User
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        user = (context as FragmentActivity).user
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            onViewStateRestored(savedInstanceState)
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+
+        binding.button.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.back_frag, DialogFragment())
+                .commit()
+        }
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        adapter = ListviewAdapter(requireContext())
+        binding.listView.adapter = adapter
+        adapter.registerDataSetObserver(object: DataSetObserver(){
+            override fun onChanged() {
+                binding.txtEmpty.visibility = if(adapter.count == 0) View.VISIBLE else View.INVISIBLE
             }
+        })
+
+        val viewModel = ListModel()
+        viewModel.getList(user)
+        viewModel.setList.observe(viewLifecycleOwner) {
+            adapter.setData(it.peekContent())
+            adapter.notifyDataSetChanged()
+        }
     }
 }
