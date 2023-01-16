3943976414590f75eb491be64e6801d20e6ad243
YangNu <bin450579@gmail.com>
2023-01-16 20:50:25 +0900

    Feat. 추가. BottomNavigationView 추가

--- app/src/main/java/com/khb/todo/FragmentActivity.kt
+++ app/src/main/java/com/khb/todo/FragmentActivity.kt
@@ -2,17 +2,30 @@ package com.khb.todo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationBarView
 import com.khb.todo.Data.DTO.User
+import com.khb.todo.databinding.ActivityFragmentBinding
 
 class FragmentActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityFragmentBinding
     lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
+        binding = ActivityFragmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         user = intent.getSerializableExtra("user") as User
         supportFragmentManager.beginTransaction()
             .replace(R.id.back_frag, ListFragment())
             .commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            when (it.itemId) {
+                R.id.menu_list -> fragmentTransaction.replace(R.id.back_frag, ListFragment()).commit()
+                R.id.menu_profile -> fragmentTransaction.replace(R.id.back_frag, ProfileFragment()).commit()
+            }
+            return@OnItemSelectedListener true
+        })
     }
 }
